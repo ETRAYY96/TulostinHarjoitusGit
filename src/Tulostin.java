cf7183d9fe3f641ae3f28233e64f3350e42aa82e
Mikael Lindén <amk1012745@student.hamk.fi>
2025-10-18 18:11:03 +0300

    Vaihdoin muuttujan arvoa varmistaakseen toimivuuuden

--- src/Tulostin.java
+++ src/Tulostin.java
@@ -1,25 +1,25 @@
 public class Tulostin {
     public static void main(String[] args) {
         
-        //muuttujat
+        //Muuttujat
         String tekija = "Mikael Linden";
-        double luku1 = 5;
+        double luku1 = 6;
         double luku2 = 2;
         double tulo = luku1 * luku2;
         double erotus = luku1 - luku2;
         double summa = luku1 + luku2;
         double jako = luku1 / luku2; 
 
-         // Ohjelman tekijä
+         //Ohjelman tekijä
         System.out.println("Hei olen Tulostin-ohjelma");
         System.out.print("Ohjelman tekijä: ");
         System.out.println(tekija);
 
-          // muuttujien arvot
+          //Muuttujien arvot
         System.out.println("Luku1-muuttujan arvo on " + luku1);
         System.out.println("Luku2-muuttujan arvo on " + luku2);
 
-         // laskujen tulokset
+         //Laskujen tulokset
         System.out.println(luku1 + " * " + luku2 + " = " + tulo);
         System.out.println(luku1 + " - " + luku2 + " = " + erotus);
         System.out.println(luku1 + " + " + luku2 + " = " + summa);
