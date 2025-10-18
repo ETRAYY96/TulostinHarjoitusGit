5ab07a62143a46e084ab96bf50cd08f8d6bc201b
Mikael Lindén <amk1012745@student.hamk.fi>
2025-10-18 18:06:11 +0300

    Siistin koodin siirtämällä muuttujat alkuun, lisäsin kommentit koodiin

--- src/Tulostin.java
+++ src/Tulostin.java
@@ -1,22 +1,25 @@
 public class Tulostin {
     public static void main(String[] args) {
-
+        
+        //muuttujat
         String tekija = "Mikael Linden";
         double luku1 = 5;
         double luku2 = 2;
-
-        System.out.println("Hei olen Tulostin-ohjelma");
-        System.out.print("Ohjelman tekijä: ");
-        System.out.println(tekija);
-
-        System.out.println("Luku1-muuttujan arvo on " + luku1);
-        System.out.println("Luku2-muuttujan arvo on " + luku2);
-
         double tulo = luku1 * luku2;
         double erotus = luku1 - luku2;
         double summa = luku1 + luku2;
         double jako = luku1 / luku2; 
 
+         // Ohjelman tekijä
+        System.out.println("Hei olen Tulostin-ohjelma");
+        System.out.print("Ohjelman tekijä: ");
+        System.out.println(tekija);
+
+          // muuttujien arvot
+        System.out.println("Luku1-muuttujan arvo on " + luku1);
+        System.out.println("Luku2-muuttujan arvo on " + luku2);
+
+         // laskujen tulokset
         System.out.println(luku1 + " * " + luku2 + " = " + tulo);
         System.out.println(luku1 + " - " + luku2 + " = " + erotus);
         System.out.println(luku1 + " + " + luku2 + " = " + summa);
